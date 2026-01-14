2990ab05e0fd6b2e694e40f3bf97a30d29bad5e1
Javier <javninpen@alu.edu.gva.es>
2026-01-14 18:23:53 +0100

    Agregar rutas para jugadores y misiones en el backend, incluyendo manejo de errores

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,53 @@ app.use(express.json())
 app.get("/", (req, res) =>{
     res.json({status: "QuestLog Corriendo"})
 })
-
+app.get("/players", async(req,res) =>{
+    try{
+        const players = await prisma.player.findMany({
+            include: {
+                quests: true
+            }
+        }) 
+        res.status(201).json(players)
+    }
+    catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+app.post("/player", async(req, res) =>{
+    try{
+        const {name} = req.body
+        const player = await prisma.player.create({
+            data: {name}
+        })
+        res.status(201).json(player)
+    }
+    catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+app.get("/quests", async(req, res) =>{
+    try{
+        const quests = await prisma.quests.findMany()
+        res.json(quests)
+    }
+    catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+app.post("/quests", async(req, res) =>{
+    try{
+        const {name, description, type, xpReward, playerId} = req.body
+        const quest = await prisma.quests.create({
+            data: {name, description, type, xpReward, playerId},
+            include: {player: true}
+        })
+        res.status(201).json(quest)
+    }
+    catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () =>{
