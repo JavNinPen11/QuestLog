effe08e87782bfd4750aa078c8cd34bebc4a21a9
Javier <javninpen@alu.edu.gva.es>
2026-01-14 21:15:01 +0100

    Agregar PrismaClient al backend y generar cliente en Dockerfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import {PrismaClient} from "@prisma/client"
 
 const app = express()
 
+
 app.use(cors())
 app.use(express.json())
+
+const prisma = new PrismaClient()
 
 app.get("/", (req, res) =>{
     res.json({status: "QuestLog Corriendo"})
