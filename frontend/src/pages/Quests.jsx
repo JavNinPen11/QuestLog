ec8970b3cd86c29fd739a7fd16d2db5555af30c6
Javier <javninpen@alu.edu.gva.es>
2026-02-13 00:43:54 +0100

    Se crea el modal de creación de quest y se integran animaciones dinamicas en este

--- frontend/src/pages/Quests.jsx
+++ frontend/src/pages/Quests.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react"
 import styles from "./Quests.module.scss"
 import { getPlayer } from "../api/player"
+import botonCrearQuest from "../assets/images/createQuestButton.png"
+
 export const Quests = () => {
     const [me, setMe] = useState()
     const [quests, setQuests] = useState()
     const [error, setError] = useState()
+    const [isOpen, setIsOpen] = useState(false)
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+        type: "DAILY"
+    })
   const getInformation = async () => {
     const res = await getPlayer()
     if(!res.message) setMe(res)
@@ -18,9 +26,50 @@ export const Quests = () => {
       setQuests(me.quests)
     }
   },[me])
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setFormData({...formData, [name]: value})
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    
+    setIsOpen(false)
+    setFormData({
+        title: "",
+        description: "",
+        type: "DAILY"
+    })
+    
+  }
     return (
         <section className={styles.wall}>
             <h1 className={styles.titleMissions}></h1>
+            <div className={styles.newQuestContainer}>
+                <button type="button" className={styles.newQuestButton} onClick={() => setIsOpen(true)}>
+                    <img src={botonCrearQuest} className={styles.newQuestImage}></img>
+                </button>
+                {isOpen && (
+                    <div className={styles.modal}>
+                        <div className={styles.modalContent}>
+                            <span className={styles.close} onClick={() => setIsOpen(false)}>
+                                &times;
+                            </span>
+                            <h2>Crear nueva Quest</h2>
+                            <form onSubmit={handleSubmit} className={styles.questForm}>
+                                <input type="text" name="title" placeholder="Título" value={formData.title} onChange={handleChange} required/>
+                                <textarea  name="description" placeholder="Descripción" value={formData.description} onChange={handleChange} required/>
+                                <select name="type" value={formData.type} onChange={handleChange}>
+                                    <option value={"DAILY"}>Diaria</option>
+                                    <option value={"MAIN"}>Principal</option>
+                                    <option value={"SIDE"}>Secundaria</option>
+                                </select>
+                                <button type="submit" className={styles.submitBtn}>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                )}
+            </div>
             <section className={styles.paper}>
                 {quests && quests.map(q => (
                     <section className={`${styles[q.type]} ${styles.card}`} key={q.id}>
